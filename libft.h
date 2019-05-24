e01b24e810c8253f8d6305c967df8fcbad0af13a
Kgotso MATULUDI <kmatulud@c4r3s3.wethinkcode.co.za>
2019-05-24 15:48:58 +0200

    added files

---
+++ libft.h
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmatulud <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/22 10:30:17 by kmatulud          #+#    #+#             */
+/*   Updated: 2019/05/24 15:45:09 by kmatulud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+
+size_t	ft_strlen(const char *s);
+void	ft_putchar(char c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_atoi(const char *str);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putnbr_fd(int nb, int fd);
+void	ft_putnbr(int nb);
+void	ft_putstr_fd(const char *str, int fd);
+void	ft_putstr(const char *str);
+char	*ft_strcat(char *dest, const char *src);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strncpy(char *dest, const char *src, size_t n);
+char	*ft_strdup(const char *src);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+size_t	ft_strlen(const char *str);
+char	*ft_strncat(char *dest, const char *src, size_t nb);
+char	*ft_strstr(const char *str, const char *to_find);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrev(char *str);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *str, const char *to_find, size_t len);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *b, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char    *ft_strcpy(char *dest, const char *src);
+
+#endif
