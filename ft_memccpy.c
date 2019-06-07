d875f26be2d440bfcafdc65d3285954c00a6d9f7
Kgotso MATULUDI <kmatulud@c4r3s3.wethinkcode.co.za>
2019-06-07 13:41:37 +0200

    added file

---
+++ ft_memccpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmatulud <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/07 10:17:22 by kmatulud          #+#    #+#             */
+/*   Updated: 2019/06/07 13:41:06 by kmatulud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	while (n--)
+	{
+		*(unsigned char *)dst++ = *(unsigned char *)src;
+		if (*(unsigned char *)src++ == (unsigned char)c)
+			return (dst);
+	}
+	return (NULL);
+}
+
+
