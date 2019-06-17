76caceb84bb6ca2b80586aacd23c291337f18676
Kgotso MATULUDI <kmatulud@c4r3s3.wethinkcode.co.za>
2019-06-17 10:36:37 +0200

    ft_memdel.c

--- ft_memdel.c
+++ ft_memdel.c
@@ -6,11 +6,10 @@
 /*   By: kmatulud <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 15:02:29 by kmatulud          #+#    #+#             */
-/*   Updated: 2019/06/14 13:17:45 by kmatulud         ###   ########.fr       */
+/*   Updated: 2019/06/17 10:36:01 by kmatulud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "libft.h"
 
 void	ft_memdel(void **ap)
