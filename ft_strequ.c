fca375d36c094b743762b6d0647a4a6b02f0ef28
Kgotso MATULUDI <kmatulud@c4r3s3.wethinkcode.co.za>
2019-06-18 12:10:27 +0200

    updated file

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,12 +6,11 @@
 /*   By: kmatulud <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/14 11:22:30 by kmatulud          #+#    #+#             */
-/*   Updated: 2019/06/14 12:06:41 by kmatulud         ###   ########.fr       */
+/*   Updated: 2019/06/18 12:09:58 by kmatulud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 int		ft_strequ(char const *s1, char const *s2)
 {
