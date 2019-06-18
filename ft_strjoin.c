6f9a5adb7083b4c83de00c4f6afcf76ec22c53b5
Kgotso MATULUDI <kmatulud@c4r3s3.wethinkcode.co.za>
2019-06-18 11:52:32 +0200

    updated files

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: kmatulud <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/14 12:22:00 by kmatulud          #+#    #+#             */
-/*   Updated: 2019/06/18 11:35:08 by kmatulud         ###   ########.fr       */
+/*   Updated: 2019/06/18 11:50:20 by kmatulud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,28 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*s3;
+    size_t	x;
+    size_t	y;
+    size_t	len;
+    char	*str;
 
-	s3 = NULL;
-	if (s1 && s2)
-	{
-		s3 = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-		if (!s3)
-			return (NULL);
-		ft_strcpy(s3, s1);
-		ft_strcat(s3, s2);
-	}
-	return (s3);
+    x = 0;
+    y = 0;
+    if (!s1 || !s2)
+        return (0);
+    len = strlen(s1) + strlen(s2) + 1;
+    str = (char *)malloc(sizeof(char) * len);
+    if (!str)
+        return (0);
+    while (s1[x] != '\0')
+    {
+        str[x] = s1[x];
+        x++;
+    }
+    while (s2[y] != '\0')
+    {
+        str[x++] = s2[y++];
+    }
+    str[x] = '\0';
+    return (str);
 }
