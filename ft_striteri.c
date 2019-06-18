7e2da98f9a6ce0041683657b090e1bda33baf887
Kgotso MATULUDI <kmatulud@c4r3s3.wethinkcode.co.za>
2019-06-18 12:11:54 +0200

    updated file

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,12 +6,11 @@
 /*   By: kmatulud <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/13 14:21:08 by kmatulud          #+#    #+#             */
-/*   Updated: 2019/06/13 16:19:02 by kmatulud         ###   ########.fr       */
+/*   Updated: 2019/06/18 12:11:34 by kmatulud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
