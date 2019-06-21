7535d54ba8f0fcf6243d80e6e7ab8d0d596212c0
Kgotso MATULUDI <kmatulud@c4r3s3.wethinkcode.co.za>
2019-06-21 11:48:48 +0200

    ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: kmatulud <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/14 12:22:00 by kmatulud          #+#    #+#             */
-/*   Updated: 2019/06/18 11:50:20 by kmatulud         ###   ########.fr       */
+/*   Updated: 2019/06/21 11:48:03 by kmatulud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
     y = 0;
     if (!s1 || !s2)
         return (0);
-    len = strlen(s1) + strlen(s2) + 1;
+    len = ft_strlen(s1) + ft_strlen(s2) + 1;
     str = (char *)malloc(sizeof(char) * len);
     if (!str)
         return (0);
